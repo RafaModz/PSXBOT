a087fbe1c67f86a36f92627618b72abc8d0af873
RafaModz <daguiladanightbot@gmail.com>
2021-02-14 10:06:19 -0300

    First commit

---
+++ src/help2.js
@@ -0,0 +1,25 @@
+const help2 = (prefix, sender) => {
+	return `help2
+
+╭──────────────╮
+ *MENU DO PS3*
+╰──────────────╯
+Link de tudo que você precisara para o PS3 Desbloqueado
+
+Como Instalar o Hen:
+https://youtu.be/r4D6Hx-jsX8
+
+Hfw 4.87: https://mega.nz/file/fAJFwSqL#PeQfhVjbGAi69S4Jr369Tly4trG4Y9ALqdlUIBzW8qk
+
+Multiman 04.85.01: https://store.brewology.com/get/homebrew.php?id=24&fid=2200
+
+Webman 1.47.34: https://store.brewology.com/get/homebrew.php?id=310&fid=2227
+(Obs: Instalem sempre a versão lite, a Full é muito pesada e pode trazer aquecimento e tela preta em alguns jogos do ps3)
+
+Homebrews: https://store.brewology.com/homebrew.php?lang=
+
+Sejam Felizes Galera, e se cuidem em cuidado com a covid ksks`
+
+}
+
+exports.help2 = help2
